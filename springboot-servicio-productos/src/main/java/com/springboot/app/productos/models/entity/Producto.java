e76cc8db907aabde18a6dfe553aca986ced3d357
mgonzalez12 <ing.marcosgonzalez1@gmail.com>
2021-08-20 22:14:03 -0500

    first commit

---
+++ springboot-servicio-productos/src/main/java/com/springboot/app/productos/models/entity/Producto.java
@@ -0,0 +1,67 @@
+package com.springboot.app.productos.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "productos")
+public class Producto implements Serializable {
+
+	
+	@Id
+	@GeneratedValue( strategy= GenerationType.IDENTITY)
+	private Long id;
+	private String nombres;
+	private Double precio;
+	
+	@Column(name = "create_at")
+	@Temporal(TemporalType.DATE)
+	private Date createAt;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public Double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8068338434639790357L;
+}
---
+++ springboot-servicio-productos/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO productos(nombres,precio,create_at) VALUES('panasonic',800,NOW());
+INSERT INTO productos(nombres,precio,create_at) VALUES('sonic',800,NOW());
+INSERT INTO productos(nombres,precio,create_at) VALUES('Apple',1000,NOW());
+INSERT INTO productos(nombres,precio,create_at) VALUES('Sony Notebook',800,NOW());
+INSERT INTO productos(nombres,precio,create_at) VALUES('Nike',800,NOW());
+INSERT INTO productos(nombres,precio,create_at) VALUES('Adidas',800,NOW());
+INSERT INTO productos(nombres,precio,create_at) VALUES('Reebok',800,NOW());
